a1d860c8172a7ca65b161f1578a14833457bca1d
BorisMejias <128161492+BorisMejias@users.noreply.github.com>
2023-04-02 12:56:26 -0400

    Versión2 "MuestraCodigo"
    -Cambiar los rectángulos por etiquetas
    -Mostar una animación de mostrar código(Cambio del color)

--- CodigoBoris/TryMuestraCodigo/src/main/java/com/mycompany/trymuestracodigo/App.java
+++ CodigoBoris/TryMuestraCodigo/src/main/java/com/mycompany/trymuestracodigo/App.java
@@ -1,52 +1,55 @@
 package com.mycompany.trymuestracodigo;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-import javafx.animation.SequentialTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.Group;
+import static javafx.application.Application.launch;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class App extends Application {
     
-    public static int recAltura = 600;
-    public static int recAncho = 600;
-    public static Color ColorFondo = Color.GREY;
+    public static int recAltura = 250;
+    public static int recAncho = 350;
+    public static Color ColorFondo = Color.BLUE;
     public static Color ColorRec = Color.RED;
     public static Color ColorLetra = Color.WHITE;
     public static Color ColorLetra2 = Color.GOLD;
+    private Label[] pasos = new Label[7];
+    private int cont = 1;
+    private int contador_indice = 0;
+    
     
     @Override
     public void start(Stage stage) {
         AnchorPane ventana = new AnchorPane();
-        Group root = new Group();
         Rectangle rec = new Rectangle();
         Rectangle rec2 = new Rectangle();
         Rectangle rec3 = new Rectangle();
         Rectangle rec4 = new Rectangle();
         Rectangle rec5 = new Rectangle();
         Rectangle rec6 = new Rectangle();
-        Scene escena = new Scene(ventana);
-        escena.setFill(ColorFondo);
-        float coorsY=50,sepa=10;
+        //Scene escena = new Scene(ventana);
+        //escena.setFill(ColorFondo);
         
         //Crear Rectángulo Grande
-        rec.setX(50);
-        rec.setY(50);
-        rec.setWidth(350);
-        rec.setHeight(300);
+        rec.setX(0);
+        rec.setY(0);
+        rec.setWidth(400);
+        rec.setHeight(400);
         rec.setFill(Color.BLUE);
         
         //Rec Pequeños
-        rec2.setX(50);
-        rec2.setY(50);
-        rec2.setWidth(340);
+        rec2.setX(0);
+        rec2.setY(0);
+        rec2.setWidth(30);
         rec2.setHeight(60);
         rec2.setFill(ColorRec);
         
@@ -75,82 +78,72 @@ public class App extends Application {
         rec6.setFill(ColorRec);
 
         //Etiquetas
-        Label nametag = new Label("mark the fisrt element as Sorted".toString());
-        Label nametag2 = new Label("for each unsorted element X\n" +
-                                                               "\n" +
-                                    "  'extract' the element X".toString());
-        Label nametag3 = new Label("for j = lastSortedIndex down to 0".toString());
-        Label nametag4 = new Label(" if current element j > X\n" +
-                                                            "\n" +
-                                    "      move sorted element to the right by 1".toString());
-        Label nametag5 = new Label("break loop and insert X here".toString());
+        pasos[0] = new Label("mark first element as sorted");
+        pasos[1] = new Label("for each unsorted element X");
+        pasos[2] = new Label("'extract' the element X");
+        pasos[3] = new Label("for j = lastSortedIndex down to 0");
+        pasos[4] = new Label("if current element j > X");
+        pasos[5] = new Label("move sorted element to the right by 1");
+        pasos[6] = new Label(" break loop and insert X here");
         
         //Color de Las Etiquetas:
-        nametag.setTextFill(ColorLetra);
-        nametag2.setTextFill(ColorLetra);
-        nametag3.setTextFill(ColorLetra);
-        nametag4.setTextFill(ColorLetra);
-        nametag5.setTextFill(ColorLetra);
+        String coloretiqueta = "-fx-background-color: red; -fx-text-fill: gold";
+        for (Label step:pasos) {
+            step.setStyle(coloretiqueta);
+        }
+        
+        //Usando TimeLine
+        VBox root = new VBox(pasos);
+        Scene escena = new Scene(ventana);
+        
+        
+        ventana.getChildren().add(root);
+        ventana.setStyle("-fx-background-color:orange");
+        
+        //root.getChildren().add(rec2);
+        Timeline grafica = new Timeline(new KeyFrame(Duration.seconds(1), event->{
+                           pasos[0].setText("mark first element as sorted ");
+                           DestacarLinea(0);      
+                           }),
+                           new KeyFrame(Duration.seconds(2),event->DestacarLinea(1)),
+                           new KeyFrame(Duration.seconds(3),event->DestacarLinea(2)), 
+                           new KeyFrame(Duration.seconds(4),event->DestacarLinea(3)),
+                           new KeyFrame(Duration.seconds(5),event->DestacarLinea(4)),
+                           new KeyFrame(Duration.seconds(6),event->DestacarLinea(5)),
+                           new KeyFrame(Duration.seconds(7),event->DestacarLinea(6)));
+        
+        grafica.setCycleCount(Timeline.INDEFINITE);
+        grafica.play();
+        
         
         //Coordenadas de Etiquetas
-        nametag.relocate(60, 70);
-        nametag2.relocate(80, 110);
-        nametag3.relocate(100, 190);
-        nametag4.relocate(120, 230);
-        nametag5.relocate(140, 310);
-        
-        
+         
+        //Tamaño letra etiqueta
+        pasos[0].setFont(new Font("Georgia",20));
+        pasos[1].setFont(new Font("Georgia",20));
+        pasos[2].setFont(new Font("Georgia",20));
+        pasos[3].setFont(new Font("Georgia",20));
+        pasos[4].setFont(new Font("Georgia",20));
+        pasos[5].setFont(new Font("Georgia",20));
+        pasos[6].setFont(new Font("Georgia",20));
         //Agregando a la Escena
         stage.setScene(escena);
         stage.setWidth(recAncho);
         stage.setHeight(recAltura);
-        ventana.getChildren().add(rec);
-        ventana.getChildren().add(rec2);
-        ventana.getChildren().add(rec3);
-        ventana.getChildren().add(rec4);
-        ventana.getChildren().add(rec5);
-        ventana.getChildren().add(rec6);
-        ventana.getChildren().add(nametag);
-        ventana.getChildren().add(nametag2);
-        ventana.getChildren().add(nametag3);
-        ventana.getChildren().add(nametag4);
-        ventana.getChildren().add(nametag5);
-        stage.show();
-        
-        
-        //Cambiando Color
-        int valor;
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Elija una opción:");
-        valor=entrada.nextInt();
-        
-        switch(valor){
-            case 2:
-                rec2.setFill(ColorLetra2);
-                stage.show();
-            break;
-            case 3:
-                rec3.setFill(ColorLetra2);
-                stage.show();
-            break;
-            case 4:
-                rec4.setFill(ColorLetra2);
-                stage.show();
-            break;
-            case 5:
-                rec5.setFill(ColorLetra2);
-                stage.show();
-            break;
-            case 6:
-                rec6.setFill(ColorLetra2);
-                stage.show();
-            break;
-            default:
-                System.out.println("Se cayó el programa");
-            break;
+        stage.setTitle("Intento Muestra Codigo");    
+        stage.show(); 
+    }
+    
+    public void DestacarLinea(int posicion){
+        if(contador_indice>=0 && contador_indice<pasos.length){
+            pasos[contador_indice].setStyle("-fx-background-color: red; -fx-text-fill: gold");
+        }
+        if(posicion>=0 && posicion<pasos.length){
+            pasos[posicion].setStyle("-fx-background-color: blue; -fx-text-fill: white");
+            contador_indice=posicion;
         }
     }
-
+    
     public static void main(String[] args) {
         launch();
     }
